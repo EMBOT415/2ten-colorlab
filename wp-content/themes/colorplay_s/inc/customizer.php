86a3541798888df50436f6381f441c11270a5ab2
EMBOT415 <embot415@gmail.com>
2017-06-19 15:37:25 -0400

    starter on _s

---
+++ wp-content/themes/colorplay_s/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * colorplay_s Theme Customizer
+ *
+ * @package colorplay_s
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function colorplay_s_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'colorplay_s_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function colorplay_s_customize_preview_js() {
+	wp_enqueue_script( 'colorplay_s_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'colorplay_s_customize_preview_js' );
